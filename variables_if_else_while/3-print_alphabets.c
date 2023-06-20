3a0f5499602339669a0abf2f0c0c593354b3dde8
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-20 03:25:09 +0000

    alphabets min

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print lowercase and uppercase.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	printf("Capital (upper) case characters;\n");
+	for (i = 'A'; i <= 'Z'; i++)
+	printf("%c ", i);
+
+	printf("\n\nLower case characters:\n");
+	for (i = 'a'; i <= 'z'; i++)
+	printf("%c ", i);
+	return (0);
+}
