8b78fa23265304a2b149196fe74a4fad64b9c304
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-20 17:12:54 +0000

    print min alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+{
+		if ((i != 'q' && i != 'e') && i <= 'z')
+			putchar(i);
+		i++;
+}
+	putchar('n');
+	return (0);
+}
