d4c7f179d324e7df1c324fe1cf835942a18a0634
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-20 18:33:40 +0000

    task 5

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,11 @@ int main(void)
 	char i = 'a';
 
 	while (i <= 'z')
+	{
 		if (i != 'q' && i != 'e')
 			putchar(i);
 		i++;
+	}
 	putchar('\n');
 	return (0);
 }
--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - single digit numbers
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	int i = 0;
-
-	for (i = 0; i < 10; i++)
+	int i;
+	for (i = 0 ; i < 10 ; i++)
 	{
 		printf("%d", i);
-		if (i < 9)
-		{
-			printf(", ");
-		}
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
