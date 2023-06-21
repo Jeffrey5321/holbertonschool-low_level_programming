7ebc5b2215ec4b4a5a226becb79e0ff26d3f130b
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-21 19:32:35 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabe - prints the alphabet, in lowercase
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a';  i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
