982b14b5c10f34b4cbf796350187a307aca93f1d
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-21 16:40:57 +0000

    numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,9 +13,9 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		if (i < 9)
-		putchar(' ');
+		if (i < '9')
 		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
