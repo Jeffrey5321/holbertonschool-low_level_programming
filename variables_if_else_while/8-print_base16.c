86c1351cf9002ff75325221a179afe7d968552bc
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-21 14:29:57 +0000

    print base

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 0; i <= 15; i++)
 	{
-		putchar(i + '0');
+		putchar(i);
 	}
 	for (i = 'a'; i <= 'f'; i++)
 	{
-		putchar(i + 'a');
+		putchar(i);
 	}
 putchar('\n');
 return (0);
