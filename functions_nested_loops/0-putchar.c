5f5443a269af1ef994a861bc45847a2802c444f6
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2023-06-21 18:49:26 +0000

    print putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	}
+	putchar ('\n');
+	return (0);
+}
