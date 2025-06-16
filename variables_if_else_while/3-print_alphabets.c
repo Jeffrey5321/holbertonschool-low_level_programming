c64badb70d11e5af79da53f65942d77853b6df28
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2025-06-16 12:13:31 -0400

    alphabet loop

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,18 @@
 int main(void)
 {
 	char c1;
+	char c2;
 
-	for (char c1 = 'a' ; c1 <= 'z' ; c1++)
+	for (c1 = 'a' ; c1 <= 'z' ; c1++)
 	{
 		putchar(c1);
 	}
-	char c2;
 
-	for (char c2 = 'A' ; c2 <= 'Z' ; c2++)
+	for (c2 = 'A' ; c2 <= 'Z' ; c2++)
 	{
 		putchar(c2);
 	}
+
 	putchar('\n');
 
 	return (0);
