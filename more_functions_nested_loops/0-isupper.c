605d520bb08e3df18f71b8fafa9af58200c338fc
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2025-06-20 12:13:23 -0400

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+# include "main.h"
+# include  <stdio.h>
+
+/**
+ * isupper - function that checks for uppercase characters.
+ * @c: the int for the parameters of my function.
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
