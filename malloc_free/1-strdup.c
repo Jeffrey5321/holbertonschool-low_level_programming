8b8785dcdb95dc87e18b967d1612859230849264
Jeffrey5321 <Jeffreysosaduarte@gmail.com>
2025-07-03 10:01:12 -0400

    duplicate

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ *@str: arg string
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	int lenght, n;
+
+	lenght = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+	arr = malloc((lenght + 1) * sizeof(char));
+	if (arr == NULL)
+	{
+		printf("error allocationg");
+		return (NULL);
+	}
+	for (n = 0; n < lenght; n++)
+	{
+		arr[n] = str[n];
+	}
+	arr[n] = '\0';
+	return (arr);
+}
